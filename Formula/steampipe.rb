f271336fd327082d23faca81ad3bcdeaebb78ba8
kai <kai@turbot.com>
2021-03-22 20:58:19 +0000

    Update for 0.3.3

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -4,12 +4,12 @@
 class Steampipe < Formula
   desc "Steampipe exposes APIs and services as a high-performance relational database, giving you the ability to write SQL-based queries to explore, assess and report on dynamic data."
   homepage "https://steampipe.io/"
-  version "0.3.2"
+  version "0.3.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/turbot/steampipe/releases/download/v0.3.2/steampipe_darwin_amd64.zip"
-    sha256 "7efbe743c910aef63d2ca782c2df37a9ce5efb3b16d622a6ef5e385828e06024"
+    url "https://github.com/turbot/steampipe/releases/download/v0.3.3/steampipe_darwin_amd64.zip"
+    sha256 "f4df9b414fbeb3b6be2b04f9a04773993140546f5e1b7be05ad94a8b27804234"
   end
 
   def install
