d5133eed285cd7756635c5464e7f850b5bc8eb46
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2021-09-24 18:29:21 +0530

    Add support for Linux. Closes #2

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -11,6 +11,10 @@ class Steampipe < Formula
     url "https://github.com/turbot/steampipe/releases/download/v0.8.2/steampipe_darwin_amd64.zip"
     sha256 "072ec5f3cee649ae1051071e485058191283c8dfd23f42dd159d9345ee880ebc"
   end
+  if OS.linux?
+    url "https://github.com/turbot/steampipe/releases/download/v0.8.2/steampipe_linux_amd64.tar.gz"
+    sha256 "a990b6f9ed355e4ab014e213030d47b0f8b483f1a5d0ec8ffd6e93c8048bc212"
+  end
 
   def install
     bin.install "steampipe"
