d5db10fb1bbb647517296badab9989d836e6543a
kai <kai@turbot.com>
2021-09-28 17:25:09 +0100

    v0.8.3

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -8,12 +8,12 @@ class Steampipe < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/turbot/steampipe/releases/download/v0.8.2/steampipe_darwin_amd64.zip"
-    sha256 "072ec5f3cee649ae1051071e485058191283c8dfd23f42dd159d9345ee880ebc"
+    url "https://github.com/turbot/steampipe/releases/download/v0.8.3/steampipe_darwin_amd64.zip"
+    sha256 "4076d120e3353ecdbdb2e95b83732d2490404db35297d05ac97c78256f5aa683"
   end
   if OS.linux?
-    url "https://github.com/turbot/steampipe/releases/download/v0.8.2/steampipe_linux_amd64.tar.gz"
-    sha256 "a990b6f9ed355e4ab014e213030d47b0f8b483f1a5d0ec8ffd6e93c8048bc212"
+    url "https://github.com/turbot/steampipe/releases/download/v0.8.3/steampipe_linux_amd64.tar.gz"
+    sha256 "2ba9837d2d9d88ed6452f27e3a27bf768906a5cac44c1c664d13954c403ea947"
   end
 
   def install
