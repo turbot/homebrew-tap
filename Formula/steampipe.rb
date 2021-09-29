1636e136e30eff5860ee703484795d0a889fa7ce
kai <kai@turbot.com>
2021-09-29 21:27:26 +0100

    v0.8.4.

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -4,16 +4,16 @@
 class Steampipe < Formula
   desc "Steampipe exposes APIs and services as a high-performance relational database, giving you the ability to write SQL-based queries to explore, assess and report on dynamic data."
   homepage "https://steampipe.io/"
-  version "0.8.3"
+  version "0.8.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/turbot/steampipe/releases/download/v0.8.3/steampipe_darwin_amd64.zip"
-    sha256 "4076d120e3353ecdbdb2e95b83732d2490404db35297d05ac97c78256f5aa683"
+    url "https://github.com/turbot/steampipe/releases/download/v0.8.4/steampipe_darwin_amd64.zip"
+    sha256 "bd672460503c4c283b56cf0395963930e961231ea8b571f9199be74e9c9e1ec2"
   end
   if OS.linux?
-    url "https://github.com/turbot/steampipe/releases/download/v0.8.3/steampipe_linux_amd64.tar.gz"
-    sha256 "2ba9837d2d9d88ed6452f27e3a27bf768906a5cac44c1c664d13954c403ea947"
+    url "https://github.com/turbot/steampipe/releases/download/v0.8.4/steampipe_linux_amd64.tar.gz"
+    sha256 "e023042e2492dd39288e74c596bfc72ab809f00649c0ed9dff3b5efd89bede46"
   end
 
   def install
