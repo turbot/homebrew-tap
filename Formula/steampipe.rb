65482f3e4524d55831202fb8da10f724da3fea48
kai <kai@turbot.com>
2021-10-07 18:49:03 +0100

    Fix handling of null unicode chars in JSON fields.  Fix issue where queries with a`limit` clause not always listing all results.

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -4,16 +4,16 @@
 class Steampipe < Formula
   desc "Steampipe exposes APIs and services as a high-performance relational database, giving you the ability to write SQL-based queries to explore, assess and report on dynamic data."
   homepage "https://steampipe.io/"
-  version "0.8.4"
+  version "0.8.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/turbot/steampipe/releases/download/v0.8.4/steampipe_darwin_amd64.zip"
-    sha256 "bd672460503c4c283b56cf0395963930e961231ea8b571f9199be74e9c9e1ec2"
+    url "https://github.com/turbot/steampipe/releases/download/v0.8.5/steampipe_darwin_amd64.zip"
+    sha256 "9b520a8c2d31a98e473d1860e74db719ae216c14add7d28fe973219fde53c794"
   end
   if OS.linux?
-    url "https://github.com/turbot/steampipe/releases/download/v0.8.4/steampipe_linux_amd64.tar.gz"
-    sha256 "e023042e2492dd39288e74c596bfc72ab809f00649c0ed9dff3b5efd89bede46"
+    url "https://github.com/turbot/steampipe/releases/download/v0.8.5/steampipe_linux_amd64.tar.gz"
+    sha256 "87310280013719893b4d1bd70df9df2d5ba070346dd33bad5e3b2e44e8ae56af"
   end
 
   def install
