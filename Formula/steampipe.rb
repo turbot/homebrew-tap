6d840c90566aee3d390141aab4b92791fe64c3c5
kai <kai@turbot.com>
2021-10-20 10:08:15 +0100

    remove deprecated "bottle :unneeded"

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -5,7 +5,6 @@ class Steampipe < Formula
   desc "Steampipe exposes APIs and services as a high-performance relational database, giving you the ability to write SQL-based queries to explore, assess and report on dynamic data."
   homepage "https://steampipe.io/"
   version "0.8.5"
-  bottle :unneeded
 
   if OS.mac?
     url "https://github.com/turbot/steampipe/releases/download/v0.8.5/steampipe_darwin_amd64.zip"
