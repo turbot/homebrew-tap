3d342ccbbcd4ab1ab50ca89156657db7e2777fc0
kai <kai@turbot.com>
2022-03-18 21:41:42 +0000

    v0.13.2

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -8,11 +8,11 @@ class Steampipe < Formula
 
   if OS.mac?
     url "https://github.com/turbot/steampipe/releases/download/v0.13.2/steampipe_darwin_amd64.zip"
-    sha256 "8d47e8d77594aa63d080f2491acf17c90207e9de55b5243c649cbf80ec61591d"
+    sha256 "7120848d8ef261dd5d211c01bced753c74732d08f05f153430e24521cdeb1ce7"
   end
   if OS.linux?
     url "https://github.com/turbot/steampipe/releases/download/v0.13.2/steampipe_linux_amd64.tar.gz"
-    sha256 "09bd0df815f94cd09e5721900744745d1f98eda13a18a87a7d9c95793c965f94"
+    sha256 "84bb69ef5f7994c65d6fc5207e0f6ae40e7c0302f2adeff9f6ea8122cc482598"
   end
 
   def install
