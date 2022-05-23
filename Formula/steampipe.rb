4cf27b13475e7241b8c92d4420a8dc58f34c43ae
kai <kai@turbot.com>
2022-05-23 16:03:36 +0100

    v0.14.5

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -4,23 +4,23 @@
 class Steampipe < Formula
   desc "Steampipe exposes APIs and services as a high-performance relational database, giving you the ability to write SQL-based queries to explore, assess and report on dynamic data."
   homepage "https://steampipe.io/"
-  version "0.14.4"
+  version "0.14.5"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/turbot/steampipe/releases/download/v0.14.4/steampipe_darwin_amd64.zip"
-    sha256 "47528322c7f3e14748eecf4d1ad092c8be8955c028103e18eda4627dceeb2991"
+    url "https://github.com/turbot/steampipe/releases/download/v0.14.5/steampipe_darwin_amd64.zip"
+    sha256 "d32f5af009a76e112d5a6e855143b8eeafce38a8abb23b9618ee2f4f4e832894"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/turbot/steampipe/releases/download/v0.14.4/steampipe_darwin_arm64.zip"
-    sha256 "b4b0a1c1ee199236d6e2870fe03a8b40bb6ee4c0f742008310c59461e64de5bf"
+    url "https://github.com/turbot/steampipe/releases/download/v0.14.5/steampipe_darwin_arm64.zip"
+    sha256 "cee954cdd536e3f67876e6c349fd353509d551c351449f992cffa4dac78d3343"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/turbot/steampipe/releases/download/v0.14.4/steampipe_linux_amd64.tar.gz"
-    sha256 "d5a772bbfc9981818c69f70c99de38c847486e7351b000cdf1ae11991f6eaa77"
+    url "https://github.com/turbot/steampipe/releases/download/v0.14.5/steampipe_linux_amd64.tar.gz"
+    sha256 "26f37f1f818bcc3e01938ad7c058670d557507a79b92ba8bc843a91a8973f2e9"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/turbot/steampipe/releases/download/v0.14.4/steampipe_linux_arm64.tar.gz"
-    sha256 "e9c0d7e4f25122bf13c2031fd8b774054cdc78643ed80e10853b4f945ea230cd"
+    url "https://github.com/turbot/steampipe/releases/download/v0.14.5/steampipe_linux_arm64.tar.gz"
+    sha256 "d0b4351a752a010767a342cb7561ad2b3a51ee0a23b25a68011c13997d30e1a2"
   end
 
   def install
