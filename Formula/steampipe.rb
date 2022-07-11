1b5ffac3d3e514e72b74b9d465a4f6be0a019300
kai <kai@turbot.com>
2022-07-11 10:36:50 +0100

    v0.15.1

--- Formula/steampipe.rb
+++ Formula/steampipe.rb
@@ -7,19 +7,19 @@ class Steampipe < Formula
   version "0.15.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/turbot/steampipe/releases/download/v0.15.0/steampipe_darwin_amd64.zip"
+    url "https://github.com/turbot/steampipe/releases/download/v0.15.1/steampipe_darwin_amd64.zip"
     sha256 "67fb069b9dab1d90099dcc7198f0c468b8d7be123ab1a0fc75f9f0daacf1377b"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/turbot/steampipe/releases/download/v0.15.0/steampipe_darwin_arm64.zip"
+    url "https://github.com/turbot/steampipe/releases/download/v0.15.1/steampipe_darwin_arm64.zip"
     sha256 "4d1a7d0561f340c26c61e2678fa35ee29a1954fe9a147400559be96bb64d4015"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/turbot/steampipe/releases/download/v0.15.0/steampipe_linux_amd64.tar.gz"
+    url "https://github.com/turbot/steampipe/releases/download/v0.15.1/steampipe_linux_amd64.tar.gz"
     sha256 "9cb32c6f1f26a23b0a81829ccbc9a37522f9c7f05fd98e30c08d9729e43fc4d1"
   end
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/turbot/steampipe/releases/download/v0.15.0/steampipe_linux_arm64.tar.gz"
+    url "https://github.com/turbot/steampipe/releases/download/v0.15.1/steampipe_linux_arm64.tar.gz"
     sha256 "f28fa7105480401801f833e81852e95dba92c90403ec8be28ef254e9767b655f"
   end
 
