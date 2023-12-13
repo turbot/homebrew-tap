2494498bbd0e47c08b248d7a741a69ce6a96684b
Victor Hadianto <victor@turbot.com>
2023-12-13 21:43:56 +1100

    Added Flowpipe formula versioning script.

--- scripts/formula_versioning_flowpipe.sh
+++ scripts/formula_versioning_flowpipe.sh
@@ -7,7 +7,7 @@ NEW_VERSION=${VERSION#"v"}
 echo $NEW_VERSION
 
 # Save file names in variables
-base_formula_name="steampipe"
+base_formula_name="flowpipe"
 base_file="Formula/$base_formula_name.rb"
 new_file="Formula/$base_formula_name@$NEW_VERSION.rb"
 
@@ -15,18 +15,18 @@ echo "Creating versioned formula for new version: $NEW_VERSION"
 
 # Verify expected Environment variables are set
 if [ -z "$NEW_VERSION" ] ; then
-    echo "Must specify next steampipe version via VERSION"
+    echo "Must specify next flowpipe version via VERSION"
     exit 1
 fi
 
-# Remove the existing base file(steampipe.rb)
+# Remove the existing base file(flowpipe.rb)
 rm -f "$base_file"
 
-# Copy the new versioned file(steampipe@x.y.z.rb) to the new base file
+# Copy the new versioned file(flowpipe@x.y.z.rb) to the new base file
 cp "$new_file" "$base_file"
 
 # Remove dots from new version
 brew_new_version="$(echo "$NEW_VERSION" | sed 's/\.//g')"
 
 # Remove versions in class name from base file
-sed  -i 's/'"class SteampipeAT$brew_new_version <"'/class Steampipe </' "$base_file"
+sed  -i 's/'"class flowpipeAT$brew_new_version <"'/class flowpipe </' "$base_file"
